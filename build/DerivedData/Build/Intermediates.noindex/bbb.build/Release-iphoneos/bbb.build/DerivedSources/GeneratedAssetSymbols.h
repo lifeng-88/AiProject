e185273cd9bc9f2189083762ef5b5e52b9985feb
lifeng <1284434959@qq.com>
2026-05-01 17:51:06 +0800

    1.1111

---
+++ build/DerivedData/Build/Intermediates.noindex/bbb.build/Release-iphoneos/bbb.build/DerivedSources/GeneratedAssetSymbols.h
@@ -0,0 +1,18 @@
+#import <Foundation/Foundation.h>
+
+#if __has_attribute(swift_private)
+#define AC_SWIFT_PRIVATE __attribute__((swift_private))
+#else
+#define AC_SWIFT_PRIVATE
+#endif
+
+/// The "LaunchScreen" asset catalog image resource.
+static NSString * const ACImageNameLaunchScreen AC_SWIFT_PRIVATE = @"LaunchScreen";
+
+/// The "UploadTipsBad" asset catalog image resource.
+static NSString * const ACImageNameUploadTipsBad AC_SWIFT_PRIVATE = @"UploadTipsBad";
+
+/// The "UploadTipsGood" asset catalog image resource.
+static NSString * const ACImageNameUploadTipsGood AC_SWIFT_PRIVATE = @"UploadTipsGood";
+
+#undef AC_SWIFT_PRIVATE
